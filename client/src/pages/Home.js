f3923702b4e9f8f2d07028319548a875fe31051b
Michail Mangasarov <mangasarov@hotmail.de>
2021-08-06 17:51:24 +0200

    add leaflet map into website

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,3 +1,18 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import React from "react";
+
 export default function Home() {
-  return <div id="mapid">HI</div>;
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 }
