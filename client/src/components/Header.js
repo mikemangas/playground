3ad3af41b284c48a5cca5365fd5b5db1d4f0897e
Michail Mangasarov <mangasarov@hotmail.de>
2021-08-19 15:06:33 +0200

    add header menu toggle

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,35 @@
+import { useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import logo from "../assets/Images/swing_icon_colored.png";
 
 export default function Header() {
+  const [toggleNavigationLinks, setToggleNavigationLinks] = useState(
+    "Header__navigation--links--off"
+  );
+
+  const [toggleWrapper, setToggleWrapper] = useState(
+    "Header__navigation__wrapper--off"
+  );
+
+  const [toggleButton, setToggleButton] = useState(
+    "Header__navigation--toggle--off"
+  );
+
   if (localStorage.getItem("userId") === null) {
     return localStorage.setItem("userId", JSON.stringify(uuidv4()));
+  }
+
+  function handleOnClick() {
+    if (toggleNavigationLinks === "Header__navigation--links--off") {
+      setToggleNavigationLinks("Header__navigation--links");
+      setToggleWrapper("Header__navigation__wrapper");
+      setToggleButton("Header__navigation--toggle");
+    } else {
+      setToggleNavigationLinks("Header__navigation--links--off");
+      setToggleWrapper("Header__navigation__wrapper--off");
+      setToggleButton("Header__navigation--toggle--off");
+    }
   }
 
   return (
@@ -12,8 +37,8 @@ export default function Header() {
       <Link to="/">
         <img className="Header__logo" width="50px" src={logo} alt="logo" />
       </Link>
-      <section className="Header__navigation__wrapper">
-        <nav className="Header__navigation--links">
+      <section className={toggleWrapper}>
+        <nav className={toggleNavigationLinks}>
           <NavLink to="/">Home</NavLink>
           <NavLink to="/map">Map</NavLink>
           <NavLink to="/impressum">Impressum</NavLink>
@@ -22,7 +47,9 @@ export default function Header() {
           <NavLink to="/kontakt">Kontakt</NavLink>
           <NavLink to="/spielplatz-eintragen">Spielplatz Eintragen</NavLink>
         </nav>
-        <div className="Header__navigation--toggle">X</div>
+        <div onClick={handleOnClick} className={toggleButton}>
+          X
+        </div>
       </section>
     </header>
   );
