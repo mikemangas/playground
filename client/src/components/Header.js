c108f5c600a8d0210fd63aec92481e45948187e2
Michail Mangasarov <mangasarov@hotmail.de>
2021-08-19 14:03:25 +0200

    add logo placeholder icon, minor header styling

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
+import logo from "../assets/Images/swing_icon_colored.png";
 
 export default function Header() {
   if (localStorage.getItem("userId") === null) {
@@ -7,14 +8,17 @@ export default function Header() {
   }
 
   return (
-    <nav className="nav">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/map">Map</NavLink>
-      <NavLink to="/impressum">Impressum</NavLink>
-      <NavLink to="/datenschutz">Datenschutz</NavLink>
-      <NavLink to="/wir">Über Uns</NavLink>
-      <NavLink to="/kontakt">Kontakt</NavLink>
-      <NavLink to="/spielplatz-eintragen">Spielplatz Eintragen</NavLink>
-    </nav>
+    <header className="Header">
+      <img className="Header__logo" width="50px" src={logo} alt="logo" />
+      <nav className="Header__navigation">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/map">Map</NavLink>
+        <NavLink to="/impressum">Impressum</NavLink>
+        <NavLink to="/datenschutz">Datenschutz</NavLink>
+        <NavLink to="/wir">Über Uns</NavLink>
+        <NavLink to="/kontakt">Kontakt</NavLink>
+        <NavLink to="/spielplatz-eintragen">Spielplatz Eintragen</NavLink>
+      </nav>
+    </header>
   );
 }
