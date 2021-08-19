dbaa910eab42a0d022e7fec2edcb5b600a166ed3
Michail Mangasarov <mangasarov@hotmail.de>
2021-08-19 14:34:42 +0200

    setup header grid

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import logo from "../assets/Images/swing_icon_colored.png";
 
@@ -9,16 +9,21 @@ export default function Header() {
 
   return (
     <header className="Header">
-      <img className="Header__logo" width="50px" src={logo} alt="logo" />
-      <nav className="Header__navigation">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/map">Map</NavLink>
-        <NavLink to="/impressum">Impressum</NavLink>
-        <NavLink to="/datenschutz">Datenschutz</NavLink>
-        <NavLink to="/wir">Über Uns</NavLink>
-        <NavLink to="/kontakt">Kontakt</NavLink>
-        <NavLink to="/spielplatz-eintragen">Spielplatz Eintragen</NavLink>
-      </nav>
+      <Link to="/">
+        <img className="Header__logo" width="50px" src={logo} alt="logo" />
+      </Link>
+      <section className="Header__navigation__wrapper">
+        <nav className="Header__navigation--links">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/map">Map</NavLink>
+          <NavLink to="/impressum">Impressum</NavLink>
+          <NavLink to="/datenschutz">Datenschutz</NavLink>
+          <NavLink to="/wir">Über Uns</NavLink>
+          <NavLink to="/kontakt">Kontakt</NavLink>
+          <NavLink to="/spielplatz-eintragen">Spielplatz Eintragen</NavLink>
+        </nav>
+        <div className="Header__navigation--toggle">X</div>
+      </section>
     </header>
   );
 }
