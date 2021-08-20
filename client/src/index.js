744eda3dc60e1f8fc119e9d57be874ca08d3fba4
Michail Mangasarov <mangasarov@hotmail.de>
2021-08-20 13:49:24 +0200

    move base colors an fonts into index.css

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import "./styles/_base.css";
-import "./styles/_colors.css";
-import "./styles/_fonts.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
