13901336a7b1342b11afd974644072591e73fb14
Michail Mangasarov <mangasarov@hotmail.de>
2021-08-23 14:00:34 +0200

    add react toast and style a bit

--- client/src/pages/Map.js
+++ client/src/pages/Map.js
@@ -9,6 +9,7 @@ import iconColored from "../assets/Images/swing_icon_colored.png";
 import iconWhite from "../assets/Images/swing_icon_white.png";
 import iconChild from "../assets/Images/child_icon.png";
 import "leaflet-loading";
+import { ToastContainer, toast } from "react-toast";
 
 export default function Map() {
   const [map, setMap] = useState(null);
@@ -67,8 +68,16 @@ export default function Map() {
       .then((userStatus) => {
         if (userStatus.status === "CHECKED-IN") {
           setPlaygroundWhereUserIsCheckedIn(clickedPlayground);
+          toast("Eingecheckt", {
+            backgroundColor: "#04aa6d",
+            color: "#fafcfb",
+          });
         } else {
           setPlaygroundWhereUserIsCheckedIn(null);
+          toast("Ausgecheckt", {
+            backgroundColor: "#dc143c",
+            color: "#fafcfb",
+          });
         }
       })
       .catch((error) => {
@@ -101,6 +110,7 @@ export default function Map() {
 
   return (
     <>
+      <ToastContainer position={"bottom-center"} />
       <SubmitForm
         className={"Map__submitform"}
         handleOnSubmit={handleOnSubmit}
@@ -153,7 +163,7 @@ export default function Map() {
                     <img
                       className="Map__Popup__child-icon"
                       src={iconChild}
-                      alt="Kinderanzahl"
+                      alt="child-counter"
                     />
                     <p className="Map__Popup__childnumber">
                       {positionData?.checkedIn?.length}
