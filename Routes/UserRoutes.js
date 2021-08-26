795d709cd968448fd33ff6fdda9332d2ae6fe559
Michail Mangasarov <mangasarov@hotmail.de>
2021-08-26 14:44:40 +0200

    jj

--- Routes/UserRoutes.js
+++ Routes/UserRoutes.js
@@ -2,36 +2,6 @@ const User = require("../models/User");
 const express = require("express");
 const router = express.Router();
 
-//Create a new User
-router.post("/api/user", (req, res) => {
-  User.create(req.body)
-    .then((newUser) => {
-      res.status(201).send(newUser);
-      console.log("User successfully generated");
-    })
-    .catch(() => {
-      res.status(500).json({
-        error:
-          "something went wrong when creating a user, please check the error",
-      });
-    });
-});
-
-//Delete a User
-router.delete("/api/user/:id", (req, res) => {
-  const id = req.params.id;
-  User.findByIdAndRemove(id)
-    .then((body) => {
-      res.status(204);
-      console.log(`User successfully deleted`);
-    })
-    .catch(() => {
-      res.status(500).json({
-        error: "something went wrong when deleting a user. please check",
-      });
-    });
-});
-
 //find out the playgroundinfos, where user is checked in
 router.get("/api/user/:userId", (req, res) => {
   const { userId } = req.params;
@@ -65,18 +35,6 @@ router.get("/api/user", (req, res) => {
           "something went wrong when calling the playgrounds. please try again",
       });
     });
-});
-
-//Find all users, checked in a specific playground
-router.get("/api/userinplayground/:checkedInPlayground", (req, res) => {
-  const { checkedInPlayground } = req.params;
-  User.find({
-    checkedInPlayground: checkedInPlayground,
-  }).then((users) => {
-    res.send({
-      userCounter: users.length,
-    });
-  });
 });
 
 module.exports = router;
