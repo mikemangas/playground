ad289eaf49734126f6c6f8499327aec8294cdffc
Michail Mangasarov <mangasarov@hotmail.de>
2021-10-03 23:13:42 +0300

    remove console log in helmetjs

--- client/src/hooks/helmet.js
+++ client/src/hooks/helmet.js
@@ -1,12 +1,6 @@
 import { Helmet } from "react-helmet";
 
 export default function helmet(title, description) {
-  const deslngth = description.length;
-
-  console.log(
-    `Max soll 120-140 sein. Der aktuelle SERP.length ist ${deslngth}`
-  );
-
   return (
     <Helmet>
       <title>{`Spielplatzchecken.de - ${title}`}</title>
