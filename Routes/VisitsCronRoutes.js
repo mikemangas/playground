d90582f20d33418075cb9e393fa83e68d8628c8b
Michail Mangasarov <mangasarov@hotmail.de>
2021-10-05 21:35:02 +0300

    add cronjobs for daily and monthly basis

--- Routes/VisitsCronRoutes.js
+++ Routes/VisitsCronRoutes.js
@@ -3,14 +3,28 @@ const router = express.Router();
 const cron = require("node-cron");
 const Visits = require("../models/Visits");
 const VisitsSummaryDaily = require("../models/VisitsSummaryDaily");
+const VisitsSummaryMonthly = require("../models/VisitsSummaryMonthly");
 
-cron.schedule("1 * * * * *", async () => {
-  const hallo = await Visits.find();
-  const baba = hallo.map((elements) => {
-    console.log(elements.counter);
-    console.log(elements.pageName);
+cron.schedule("59 59 20 * * *", async () => {
+  const currentVisits = await Visits.find();
+  const allVisits = currentVisits.map((singleValues) => {
+    VisitsSummaryDaily.create({
+      counter: singleValues.counter,
+      pageName: singleValues.pageName,
+    });
   });
-  return baba;
+  return allVisits;
+});
+
+cron.schedule("0 0 1 * 1-12 *", async () => {
+  const currentVisits = await Visits.find();
+  const allVisits = currentVisits.map((singleValues) => {
+    VisitsSummaryMonthly.create({
+      counter: singleValues.counter,
+      pageName: singleValues.pageName,
+    });
+  });
+  return allVisits;
 });
 
 module.exports = router;
