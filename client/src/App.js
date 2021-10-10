be3e5a1f3d5e4c533301858bf7da42864e99aa20
Michail Mangasarov <mangasarov@hotmail.de>
2021-10-10 14:51:57 +0300

    fix doublecount on load

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,11 @@
 import Main from "./components/Main";
 import Header from "./components/Header";
 import "./App.css";
-import { useEffect, useState } from "react";
-import defaultVisitsPatch from "./hooks/defaultVisitsPatch";
+import { useState } from "react";
 
 function App() {
   const [checkInState, setCheckInState] = useState();
   const [checkOutState, setCheckOutState] = useState();
-
-  useEffect(() => {
-    defaultVisitsPatch("615af5a7ff20382e9dd25aac");
-  }, []);
 
   return (
     <div className="App">
